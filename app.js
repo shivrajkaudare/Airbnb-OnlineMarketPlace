f497241c1763bbc13dde67ed827fd1ecb9b39fa4
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-07-23 19:09:02 +0530

    ejs-mate boilerplate templating

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
 // connection with Database
 const MONGO_URL = "mongodb://127.0.0.1:27017/wonderlust";
 main()
@@ -22,6 +24,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
 
 app.get("/", (req, res) => {
   res.send("hello iam a root");
