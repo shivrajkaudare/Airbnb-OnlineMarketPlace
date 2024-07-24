4f0a4ad5c923dc1988e38cc05f19745e92743389
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-07-24 18:03:59 +0530

    NavBar.ejs

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
   res.send("hello iam a root");
